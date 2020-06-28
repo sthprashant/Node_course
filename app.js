b1629291f8a5125e0d7bf4b68d5e89ba7eae627d
Prashant Shrestha <sthprashant@gmail.com>
2020-06-28 22:24:09 +1000

    express 60 and 61

--- app.js
+++ app.js
@@ -8,13 +8,29 @@ OS
 */
 
 const http = require('http');
+const express = require('express');
 
-function rqListener(req, res){
+const app = express (); // initialising express object
 
-}
-
-http.createServer((req,res) => {
-    console.log(req);
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next(); // allows the request to continue to the next middleware in line
 }); 
+//add a new middleware function, accepts array of req handlers and other usecases. 
+//next is a funtion that weill be passed to call back to allow the request to travel to the next request
+
+app.use((req, res, next) => {
+    console.log('in another middleware');
+
+    //sending the response
+    res.send('<h1> hello from express </h1>'); 
+    // send automatically set the content type
+    //can set ,manually using res.setHeader()
+}); 
+
+
+const server = http.createServer(app);
+server.listen(3000);
+
 
 
