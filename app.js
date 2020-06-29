2c2982b9c8352e659df536217300858c9bd1d24d
Prashant Shrestha <sthprashant@gmail.com>
2020-06-29 23:13:11 +1000

    71 done

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
-/*
-Core modules:
-HTTPS- Launch a SSL server
-HTTP - Launch a server, send request, communicate with other servers
-FS - File handling
-PATH
-OS
-*/
+const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const http = require('http');
-const express = require('express');
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
 
-const app = express (); // initialising express object
+const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next(); // allows the request to continue to the next middleware in line
-}); 
-//add a new middleware function, accepts array of req handlers and other usecases. 
-//next is a funtion that weill be passed to call back to allow the request to travel to the next request
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log('in another middleware');
+  res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+});
 
-    //sending the response
-    res.send('<h1> hello from express </h1>'); 
-    // send automatically set the content type
-    //can set ,manually using res.setHeader()
-}); 
-
-
-const server = http.createServer(app);
-server.listen(3000);
-
-
-
+app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require('path'); // import path module
+const express = require('express');
+
+const router = express.Router(); // calling express router
+
+router.get('/add-product', (req, res, next) => {
+    //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views','add-product.html'))
+  });
+  
+  router.post('/add-product', (req, res, next) => {
+      console.log(req.body);
+      res.redirect('/');
+  });
+  
+
+module.exports = router;
